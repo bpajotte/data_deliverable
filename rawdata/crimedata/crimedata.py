507374c2d22827ce60add134b1ec4de649ccb594
stephenarosaj <stephenarosaj@gmail.com>
2023-03-21 18:33:05 -0400

    ready to start joining crime data

--- rawdata/crimedata/crimedata.py
+++ rawdata/crimedata/crimedata.py
@@ -1,12 +1,14 @@
 import pandas
 
-# source for data: https://stacks.stanford.edu/file/druid:yg821jf8611/yg821jf8611_ma_statewide_2020_04_01.csv.zip, MA State Patrol
-# filepath = 
+# source for data: https://stacks.stanford.edu/file/druid:yg821jf8611/yg821jf8611_ma_statewide_2020_04_01.csv.zip
+# data readme: https://github.com/stanford-policylab/opp/blob/master/data_readme.md#statewide-ma
 
-csv = pandas.read_csv("ma_statewide_2020_04_01.csv")
-nrows, ncols = csv.shape
-print("there are " + str(nrows) + " rows and " + str(ncols) + " cols in raw csv")
+# pull data into pandas
+csv_stanford = pandas.read_csv("rawdata\crimedata\openpolicingstanfordedu\ma-state-patrol.csv")
+nrows_stanford, ncols_stanford = csv_stanford.shape
+print("there are " + str(nrows_stanford) + " rows and " + str(ncols_stanford) + " cols in raw csv")
+# filter by boston
+csv_stanford = csv_stanford[csv_stanford['location'] == "BOSTON"]
+nrows_stanford, ncols_stanford = csv_stanford.shape
+print("there are " + str(nrows_stanford) + " rows and " + str(ncols_stanford) + " cols in filtered data")
 
-filtered_data = csv[csv['location'] == "BOSTON"]
-nrows, ncols = filtered_data.shape
-print("there are " + str(nrows) + " rows and " + str(ncols) + " cols in filtered data")
