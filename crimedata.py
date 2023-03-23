cf0009fbfc60625832e95c9f415466cbbe2df995
stephenarosaj <stephenarosaj@gmail.com>
2023-03-22 20:30:28 -0400

    brUh

--- crimedata.py
+++ crimedata.py
@@ -1,10 +1,16 @@
 import pandas
 
-# source for data: https://stacks.stanford.edu/file/druid:yg821jf8611/yg821jf8611_ma_statewide_2020_04_01.csv.zip
-# data readme: https://github.com/stanford-policylab/opp/blob/master/data_readme.md#statewide-ma
+
+
+
+# source: https://data.boston.gov/dataset/crime-incident-reports-august-2015-to-date-source-new-system
+# readme: https://data.boston.gov/dataset/crime-incident-reports-august-2015-to-date-source-new-system/resource/9c30453a-fefa-4fe0-b51a-5fc09b0f4655
+
+# source: https://openpolicing.stanford.edu/data/
+# readme: https://github.com/stanford-policylab/opp/blob/master/data_readme.md#statewide-ma
 
 # pull data into pandas 
-csv_stanford = pandas.read_csv("data/crime_data/openpolicingstanfordedu/ma-state-patrol.csv")
+csv_stanford = pandas.read_csv("data/raw_data/crime_data/openpolicingstanfordedu/ma_statewide_2006-2015.csv")
 nrows_stanford, ncols_stanford = csv_stanford.shape
 print("there are " + str(nrows_stanford) + " rows and " + str(ncols_stanford) + " cols in raw csv_stanford")
 # filter by boston
@@ -13,34 +19,34 @@ nrows_stanford, ncols_stanford = csv_stanford.shape
 print("there are " + str(nrows_stanford) + " rows and " + str(ncols_stanford) + " cols in filtered csv_stanford")
 
 
-crime_reports_2015 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2015.csv")
+crime_reports_2015 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2015.csv")
 nrows_cr_15, ncols_cr_15 = crime_reports_2015.shape
 print("there are " + str(nrows_cr_15) + " rows and " + str(ncols_cr_15) + " cols in crime_reports_2015")
 
-crime_reports_2016 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2016.csv")
+crime_reports_2016 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2016.csv")
 nrows_cr_16, ncols_cr_16 = crime_reports_2016.shape
 print("there are " + str(nrows_cr_16) + " rows and " + str(ncols_cr_16) + " cols in crime_reports_2016")
 
-crime_reports_2017 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2017.csv")
+crime_reports_2017 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2017.csv")
 nrows_cr_17, ncols_cr_17 = crime_reports_2017.shape
 print("there are " + str(nrows_cr_17) + " rows and " + str(ncols_cr_17) + " cols in crime_reports_2017")
 
-crime_reports_2018 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2018.csv")
+crime_reports_2018 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2018.csv")
 nrows_cr_18, ncols_cr_18 = crime_reports_2018.shape
 print("there are " + str(nrows_cr_18) + " rows and " + str(ncols_cr_18) + " cols in crime_reports_2018")
 
-crime_reports_2019 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2019.csv")
+crime_reports_2019 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2019.csv")
 nrows_cr_19, ncols_cr_19 = crime_reports_2019.shape
 print("there are " + str(nrows_cr_19) + " rows and " + str(ncols_cr_19) + " cols in crime_reports_2019")
 
-crime_reports_2020 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2020.csv")
+crime_reports_2020 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2020.csv")
 nrows_cr_20, ncols_cr_20 = crime_reports_2020.shape
 print("there are " + str(nrows_cr_20) + " rows and " + str(ncols_cr_20) + " cols in crime_reports_2020")
 
-crime_reports_2021 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2021.csv")
+crime_reports_2021 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2021.csv")
 nrows_cr_21, ncols_cr_21 = crime_reports_2021.shape
 print("there are " + str(nrows_cr_21) + " rows and " + str(ncols_cr_21) + " cols in crime_reports_2021")
 
-crime_reports_2022 = pandas.read_csv("data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2022.csv")
+crime_reports_2022 = pandas.read_csv("data/raw_data/crime_data/databostongov-crime-incident-reports-august-2015-to-date-source-new-system/crime incident reports - 2022.csv")
 nrows_cr_22, ncols_cr_22 = crime_reports_2022.shape
 print("there are " + str(nrows_cr_22) + " rows and " + str(ncols_cr_22) + " cols in crime_reports_2022")
--- spendingdata.py
+++ spendingdata.py
@@ -1,9 +1,10 @@
 import pandas
 
-# source for data: https://data.boston.gov/dataset/checkbook-explorer
+# source: https://data.boston.gov/dataset/checkbook-explorer
+# readme: https://data.boston.gov/dataset/checkbook-explorer
 
 # pull data into pandas
-checkbook_2015 = pandas.read_csv("checkbook-explorer-fy15.csv")
+checkbook_2015 = pandas.read_csv("data/raw_data/spending_data/checkbook-explorer-fy15.csv")
 nrows_checkbok_15, ncols_checkbok_15 = checkbook_2015.shape
 print("there are " + str(nrows_checkbok_15) + " rows and " + str(ncols_checkbok_15) + " cols in raw checkbook_2015")
 
@@ -12,7 +13,7 @@ checkbook_2015 = checkbook_2015[checkbook_2015['dept_name'] == "Police Departmen
 nrows_checkbok_15, ncols_checkbok_15 = checkbook_2015.shape
 print("there are " + str(nrows_checkbok_15) + " rows and " + str(ncols_checkbok_15) + " cols in filtered checkbook_2015")
 
-checkbook_2016 = pandas.read_csv("checkbook-explorer-fy16.csv")
+checkbook_2016 = pandas.read_csv("data/raw_data/spending_data/checkbook-explorer-fy16.csv")
 nrows_checkbok_16, ncols_checkbok_16 = checkbook_2016.shape
 print("there are " + str(nrows_checkbok_16) + " rows and " + str(ncols_checkbok_16) + " cols in raw checkbook_2016")
 # filter by police department
@@ -20,7 +21,7 @@ checkbook_2016 = checkbook_2016[checkbook_2016['dept_name'] == "Police Departmen
 nrows_checkbok_16, ncols_checkbok_16 = checkbook_2016.shape
 print("there are " + str(nrows_checkbok_16) + " rows and " + str(ncols_checkbok_16) + " cols in filtered checkbook_2016")
 
-checkbook_2017 = pandas.read_csv("checkbook-explorer-fy17.csv")
+checkbook_2017 = pandas.read_csv("data/raw_data/spending_data/checkbook-explorer-fy17.csv")
 nrows_checkbok_17, ncols_checkbok_17 = checkbook_2017.shape
 print("there are " + str(nrows_checkbok_17) + " rows and " + str(ncols_checkbok_17) + " cols in raw checkbook_2017")
 # filter by police department
@@ -28,7 +29,7 @@ checkbook_2017 = checkbook_2017[checkbook_2017['dept_name'] == "Police Departmen
 nrows_checkbok_17, ncols_checkbok_17 = checkbook_2017.shape
 print("there are " + str(nrows_checkbok_17) + " rows and " + str(ncols_checkbok_17) + " cols in filtered checkbook_2017")
 
-checkbook_2018 = pandas.read_csv("checkbook-explorer-fy18.csv")
+checkbook_2018 = pandas.read_csv("data/raw_data/spending_data/checkbook-explorer-fy18.csv")
 nrows_checkbok_18, ncols_checkbok_18 = checkbook_2018.shape
 print("there are " + str(nrows_checkbok_18) + " rows and " + str(ncols_checkbok_18) + " cols in raw checkbook_2018")
 # filter by police department
@@ -36,7 +37,7 @@ checkbook_2018 = checkbook_2018[checkbook_2018['dept_name'] == "Police Departmen
 nrows_checkbok_18, ncols_checkbok_18 = checkbook_2018.shape
 print("there are " + str(nrows_checkbok_18) + " rows and " + str(ncols_checkbok_18) + " cols in filtered checkbook_2018")
 
-checkbook_2019 = pandas.read_csv("checkbook-explorer-fy19.csv")
+checkbook_2019 = pandas.read_csv("data/raw_data/spending_data/checkbook-explorer-fy19.csv")
 nrows_checkbok_19, ncols_checkbok_19 = checkbook_2019.shape
 print("there are " + str(nrows_checkbok_19) + " rows and " + str(ncols_checkbok_19) + " cols in raw checkbook_2019")
 # filter by police department
@@ -44,7 +45,7 @@ checkbook_2019 = checkbook_2019[checkbook_2019['dept_name'] == "Police Departmen
 nrows_checkbok_19, ncols_checkbok_19 = checkbook_2019.shape
 print("there are " + str(nrows_checkbok_19) + " rows and " + str(ncols_checkbok_19) + " cols in filtered checkbook_2019")
 
-checkbook_2020 = pandas.read_csv("checkbook-explorer-fy20.csv")
+checkbook_2020 = pandas.read_csv("data/raw_data/spending_data/checkbook-explorer-fy20.csv")
 nrows_checkbok_20, ncols_checkbok_20 = checkbook_2020.shape
 print("there are " + str(nrows_checkbok_20) + " rows and " + str(ncols_checkbok_20) + " cols in raw checkbook_2020")
 # filter by police department
@@ -52,7 +53,7 @@ checkbook_2020 = checkbook_2020[checkbook_2020['dept_name'] == "Police Departmen
 nrows_checkbok_20, ncols_checkbok_20 = checkbook_2020.shape
 print("there are " + str(nrows_checkbok_20) + " rows and " + str(ncols_checkbok_20) + " cols in filtered checkbook_2020")
 
-checkbook_2021 = pandas.read_csv("checkbook_explorerfy21.csv", encoding = "ISO-8859-1")
+checkbook_2021 = pandas.read_csv("data/raw_data/spending_data/checkbook_explorerfy21.csv", encoding = "ISO-8859-1")
 nrows_checkbok_21, ncols_checkbok_21 = checkbook_2021.shape
 print("there are " + str(nrows_checkbok_21) + " rows and " + str(ncols_checkbok_21) + " cols in raw checkbook_2021")
 # filter by police department
@@ -60,7 +61,7 @@ checkbook_2021 = checkbook_2021[checkbook_2021['Dept_Name'] == "Police Departmen
 nrows_checkbok_21, ncols_checkbok_21 = checkbook_2021.shape
 print("there are " + str(nrows_checkbok_21) + " rows and " + str(ncols_checkbok_21) + " cols in filtered checkbook_2021")
 
-checkbook_2022 = pandas.read_csv("checkbook_explorerfy22.csv", encoding = "ISO-8859-1")
+checkbook_2022 = pandas.read_csv("data/raw_data/spending_data/checkbook_explorerfy22.csv", encoding = "ISO-8859-1")
 nrows_checkbok_22, ncols_checkbok_22 = checkbook_2022.shape
 print("there are " + str(nrows_checkbok_22) + " rows and " + str(ncols_checkbok_22) + " cols in raw checkbook_2022")
 
